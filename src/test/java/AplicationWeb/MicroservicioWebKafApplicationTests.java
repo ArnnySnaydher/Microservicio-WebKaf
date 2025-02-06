b03a00a776918cb622982408dea54fdef08bcc3f
Arnny Snaydher <quispeloayza18@hotmail.com>
2025-02-05 22:36:48 -0500

    first commt

---
+++ src/test/java/AplicationWeb/MicroservicioWebKafApplicationTests.java
@@ -0,0 +1,13 @@
+package AplicationWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioWebKafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
